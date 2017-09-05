6cde0f50d923acfcf2fd2325d88cf3213ba05c63
Oscar Magnusson <balobba@gmail.com>
2017-09-05 15:03:15 +0200

    Added warping, added fading between zones (not perfect)

---
+++ Assets/Scripts/ScreenFader.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenFader : MonoBehaviour {
+
+    Animator anim;
+    public bool isFading = false;
+
+	void Start () {
+        anim = GetComponent<Animator> ();
+
+    }
+
+    public IEnumerator FadeToClear()
+    {
+        isFading = true;
+        anim.SetTrigger("FadeIn");
+
+        while (isFading)
+        {
+            yield return null;
+        }
+        Debug.Log("CLEAR");
+        
+    }
+
+    public IEnumerator FadeToBlack()
+    {
+        isFading = true;
+        anim.SetTrigger("FadeOut");
+
+        while (isFading)
+        {
+            yield return null;
+        }
+        Debug.Log("BLACK");
+    }
+
+    void AnimationComplete() //This function is called at the end of the animation
+    {
+        isFading = false;
+
+    }
+
+}
---
+++ Assets/Scripts/Warp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour {
+
+    public Transform warpTarget;
+
+    IEnumerator OnTriggerEnter2D(Collider2D other)
+    {
+        
+        ScreenFader sf = GameObject.FindGameObjectWithTag("Fader").GetComponent<ScreenFader> ();
+
+        Debug.Log("PRE FADE TO BLACK");
+ 
+        yield return StartCoroutine(sf.FadeToBlack());
+        Debug.Log("UPDATE PLAYER POSITION");
+        
+        //Debug.Log("collidied");
+        other.gameObject.transform.position = warpTarget.position; //moves the object that collided to the target spot
+        Camera.main.transform.position = warpTarget.position; //the camera follows to the target spot
+
+        Debug.Log("PRE FADE TO CLEAR");
+        
+        yield return StartCoroutine(sf.FadeToClear());
+    }
+
+}
