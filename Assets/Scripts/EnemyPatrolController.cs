a2c610c0b999dfd0b7e7d81d31f8d1675e7c2b2d
Oscar Magnusson <balobba@gmail.com>
2017-09-07 11:53:05 +0200

    Added first weapon to player. Added hurting script. Enemies and player dies when colliding

--- Assets/Scripts/EnemyPatrolController.cs
+++ Assets/Scripts/EnemyPatrolController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyPatrolController : MonoBehaviour {
 
@@ -17,14 +18,21 @@ public class EnemyPatrolController : MonoBehaviour {
 
     private Vector2 moveDirection;
 
+    public float waitToReload;
+    private bool reloading;
+    private GameObject thePlayer;
+
 
 	// Use this for initialization
 	void Start () {
         rbody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
 
-        timeBetweenMoveCounter = timeBetweenMove;
-        timeToMoveCounter = timeToMove;
+        //timeBetweenMoveCounter = timeBetweenMove;
+        //timeToMoveCounter = timeToMove;
+
+        timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f); //adds randomness to enemies movement
+        timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.25f);
 
     }
 	
@@ -43,7 +51,8 @@ public class EnemyPatrolController : MonoBehaviour {
             {
                 moving = false;
                 anim.SetBool("is_walking", false);
-                timeBetweenMoveCounter = timeBetweenMove;
+                //timeBetweenMoveCounter = timeBetweenMove;
+                timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
             }
 
         }
@@ -56,10 +65,39 @@ public class EnemyPatrolController : MonoBehaviour {
             if (timeBetweenMoveCounter < 0)
             {
                 moving = true;
-                timeToMoveCounter = timeToMove;
+                //timeToMoveCounter = timeToMove;
+                timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.25f);
 
                 moveDirection = new Vector2(Random.Range(-1f, 1f) * moveSpeed, Random.Range(-1f, 1f) * moveSpeed);
             }
         }
+
+
+        if (reloading)
+        {
+            waitToReload -= Time.deltaTime;
+            if (waitToReload < 0)
+            {
+                //Application.LoadLevel(Application.loadedLevel); //reloads the level and spawns player where he started
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //another way to to the same thing (loading the scene instead)
+                thePlayer.SetActive(true);
+            }
+
+        }
+
 	}
+
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            //Destroy(other.gameObject);
+            other.gameObject.SetActive(false);
+            reloading = true;
+            thePlayer = other.gameObject;
+        }
+
+    }
+
 }
---
+++ Assets/Scripts/HurtEnemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtEnemy : MonoBehaviour
+{
+
+    //public CircleCollider2D enemyBody;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        /*
+                if(other.gameObject == enemyBody)
+                {
+                    Destroy(other.gameObject); //FUNKAR INTE JUST NU
+
+                }
+                */
+
+        
+            if (other.tag == "Enemy")
+            {
+                Destroy(other.gameObject); //Kills it
+
+
+            }
+        
+        
+
+
+
+    }
+}
