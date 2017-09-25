1d970c3171363189a87a3727280c601ba0a9e04d
Oscar Magnusson <balobba@gmail.com>
2017-09-25 16:11:42 +0200

    Added bullets to the tank enemy. Bullets hurt the player and shoot in the general direction of the player. Tank and bullets are set to prefabs

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    private Rigidbody2D rbody;
+    public int speed; //4 is good for now
+    Vector2 direction;
+    Vector2 bulletDirection;
+    public int bulletDamage;
+
+    private void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+
+        rbody.AddForce(bulletDirection * speed);
+
+        Destroy(gameObject, 2);
+
+
+
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        direction = direction.normalized;
+        Debug.Log("direction: " + direction);
+        //Debug.Log("angle: " + Vector2.Angle(direction, Vector2.zero));
+
+
+        if (Vector2.Angle(direction, Vector2.up) <= 45.0)
+        {
+            bulletDirection = Vector2.up;
+            //rbody.AddForce(Vector2.up *speed);
+            Debug.Log("North");
+        }
+        else if (Vector2.Angle(direction, Vector2.right) <= 45.0)
+        {
+            bulletDirection = Vector2.right;
+            Debug.Log("East");
+        }
+        else if (Vector2.Angle(direction, Vector2.down) <= 45.0)
+        {
+            bulletDirection = Vector2.down;
+            Debug.Log("South");
+        }
+        else
+        {
+            bulletDirection = Vector2.left;
+            Debug.Log("West");
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        //add give damage to player!
+
+        if (other.gameObject.tag == "Player")
+        {
+
+            other.gameObject.GetComponent<PlayerHealthManager>().HurtPlayer(bulletDamage);
+
+            Destroy(gameObject);
+
+        }
+
+
+    }
+
+}
--- Assets/Scripts/EnemyChaseAndShootController.cs
+++ Assets/Scripts/EnemyChaseAndShootController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyChaseController : MonoBehaviour
+public class EnemyChaseAndShootController : MonoBehaviour
 {
 
     private Rigidbody2D rbody;
@@ -12,10 +12,12 @@ public class EnemyChaseController : MonoBehaviour
 
     public CircleCollider2D territory;
     bool playerInTerritory;
-    public Transform target;
+    public Transform target; //the player target
     public float speed;
     public float distanceToTarget;
 
+    public GameObject EnemyBullet;
+    public Transform cannonPosition;
 
 
     // Use this for initialization
@@ -24,8 +26,21 @@ public class EnemyChaseController : MonoBehaviour
         rbody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         playerInTerritory = false;
+
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //Invoke("FireEnemyBullet", 1f);
+
+        }
+
+
+    }
+
+   
     void OnTriggerStay2D(Collider2D other) //Everything that happens inside circle
     {
         if (other.gameObject.tag == "Player")
@@ -60,15 +75,40 @@ public class EnemyChaseController : MonoBehaviour
 
     }
 
+    void FireEnemyBullet()
+    {
+        //GameObject player = GameObject.Find("Player");
+        
+        GameObject bullet = Instantiate(EnemyBullet, cannonPosition.position, cannonPosition.rotation);
+        //bullet.transform.position = cannonPosition.position;
+        Vector2 direction = target.transform.position - bullet.transform.position;
+        bullet.GetComponent<BulletController>().SetDirection(direction);
+
+    }
+
 
 
     // Update is called once per frame
     void Update()
     {
+
+        //Debug.Log("distanse to target: " + Vector3.Distance(transform.position, target.position));
         if (playerInTerritory)
         {
             //Debug.Log("inside circle");
             MoveToPlayer();
+
+            if(Vector2.Distance(transform.position, target.position) < distanceToTarget)
+            {
+                //Vector2 moveDirection = new Vector2((transform.position.x - target.position.x) * 0.01f, (transform.position.y - target.position.y) * 0.01f);
+                //rbody.velocity = -moveDirection;
+                rbody.velocity = Vector2.zero;
+                
+                Invoke("FireEnemyBullet", 1f);
+
+            }
+            
+
 
 
         }
